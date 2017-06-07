d78fec8f3fcdf8c6f04c385bf62193ad58cd7200
LeonClover <839415938@qq.com>
2017-06-07 11:40:03 +0800

    项目架构搭建

---
+++ DouYuZB/DouYuZB/Classes/Main/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  DouYuZB
+//
+//  Created by Leon on 2017/6/7.
+//  Copyright © 2017年 pingan. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addChildVC(storyName: "Home")
+        addChildVC(storyName: "Live")
+        addChildVC(storyName: "Follow")
+        addChildVC(storyName: "Profile")
+    }
+    
+    private func addChildVC(storyName: String){
+        let homeVC = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!
+        addChildViewController(homeVC)
+    }
+}
